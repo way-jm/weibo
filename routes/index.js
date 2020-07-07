73bcee80c090ae6ba3c68976cd6a94c371b201ee
way <weizhengming@ahqianmo.com>
2020-07-07 17:25:06 +0800

    介绍router

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,35 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const {username} = ctx.params;
+  ctx.body = {
+    title: 'this is profile page',
+    username:username
+  }
+})
+
+router.get('/loadmore/:username/:pageindex', async (ctx, next) => {
+  const {username,pageindex} = ctx.params;
+  ctx.body = {
+    title: 'this is loadmore page',
+    username,
+    pageindex
+  }
+})
+
+router.post('/login',async (ctx,next)=>{
+  const{username,password}= ctx.request.body;
+  ctx.body = {
+    title:'post',
+    username,
+    password
   }
 })
 
